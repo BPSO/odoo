403b62bcc8bcfe294827459053c4aaca69eec207
BPSO SPRL <gitadmin@bpso.biz>
2025-09-23 18:53:43 +0200

    Update clean_branch.sh

--- .cleaning/clean_branch.sh
+++ .cleaning/clean_branch.sh
@@ -4,7 +4,7 @@ set -e
 echo "🔹 Nettoyage du repo pour base_framework"
 
 # 0) Modules à conserver (techniques). Ajuste la liste si besoin.
-KEEP_MODULES=("base" "web" "mail" "bus" "base_setup")
+KEEP_MODULES=("base" "web" "web_tour" "html_editor" "mail" "bus" "base_setup" "l10n_be")
 
 # 1) Supprimer tous les addons sauf ceux conservés
 for d in addons/*; do
@@ -33,9 +33,9 @@ find addons -type f -path "*/i18n/*.po" \
 # 3) Suppression ciblée de fichiers individuels
 # Liste des fichiers (chemins relatifs au repo) à supprimer
 single_files_to_delete=(
-  "odoo/addons/base/static/description/index.html"
-  "odoo/addons/web/static/description/index.html"
-  # ajoute d’autres fichiers ici si nécessaire
+  ".github/PULL_REQUEST_TEMPLATE.md"
+  ".github/ISSUE_TEMPLATE/1_bug_form.yml"
+  ".github/ISSUE_TEMPLATE/config.yml"
 )
 
 for f in "${single_files_to_delete[@]}"; do
